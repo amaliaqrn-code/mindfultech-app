8eab245af742402abd49b44c22b175ae8e83da75
zaaluuv <zaahiroh03@gmail.com>
2026-05-06 00:45:29 +0700

    regist2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mindfultech_app/pages/registrasi_page.dart';
+import 'package:mindfultech_app/presentation/auth/registrasi_page.dart';
 
 void main() {
   runApp(MyApp());
